b6335a677f910ef823c4a980dfbae9a761aa60b0
unknown <jackelyne.alicia.miranda.ramos@gmail.com>
2024-07-11 14:33:00 -0300

    python_inicio

---
+++ src/config/database.py
@@ -0,0 +1,30 @@
+import sys
+import os
+
+# Adiciona o diretório raiz do projeto ao sys.path
+current_dir = os.path.dirname(__file__)
+root_dir = os.path.abspath(os.path.join(current_dir, '../../'))
+sys.path.insert(0, root_dir)
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+from src.config.settings import settings
+
+DATABASE_URL = settings.DATABASE_URL
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
---
+++ src/main.py
@@ -0,0 +1,166 @@
+import sys
+import os
+
+# Adiciona o diretório raiz do projeto ao sys.path
+current_dir = os.path.dirname(__file__)
+root_dir = os.path.abspath(os.path.join(current_dir, '..'))
+sys.path.insert(0, root_dir)
+
+from fastapi import FastAPI, HTTPException
+from fastapi.openapi.docs import get_swagger_ui_html
+from starlette.responses import RedirectResponse, JSONResponse
+
+# Remova temporariamente a configuração de log
+# from src.config.log_config import log_config
+
+from src.controller.auth_controller import auth_router
+from src.controller.product_controller import product_router
+
+app = FastAPI(
+    title="Product Registration API",
+    description="Api to manage products",
+    version="1.0.0",
+    openapi_url="/openapi.json",
+    docs_url=None,
+    redoc_url=None,
+    contact={
+        "name": "Luan Fernandes",
+        "email": "luan.santos26@fatec.sp.gov.br",
+        "url": "https://luanfernandes.dev"
+    },
+    license_info={
+        "name": "MIT",
+        "url": "https://opensource.org/licenses/MIT"
+    },
+    servers=[
+        {
+            "url": "http://localhost:8000",
+            "description": "Development server"
+        },
+        {
+            "url": "http://user.luanfernandes.dev",
+            "description": "Production server"
+        }
+    ]
+)
+
+app.include_router(product_router)
+app.include_router(auth_router)
+
+@app.exception_handler(Exception)
+async def general_exception_handler(request, exc: Exception):
+    return JSONResponse(
+        status_code=500,
+        content={"message": "Um erro inesperado correu no servidor"}
+    )
+
+@app.exception_handler(HTTPException)
+async def general_exception_handler(request, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"message": exc.detail}
+    )
+
+@app.get('/', tags=['Redirect'], include_in_schema=False)
+async def redirect_to_docs():
+    return RedirectResponse(url='/docs')
+
+@app.get('/docs', tags=['Redirect'], include_in_schema=False)
+async def get_openapi():
+    return get_swagger_ui_html(
+        openapi_url="/openapi.json",
+        title="OpenApi UI"
+    )
+
+if __name__ == '__main__':
+    import uvicorn
+    print("Starting Uvicorn server...")
+    uvicorn.run(app, host='localhost', port=8000)  # Removido log_config
+    print("Uvicorn server has stopped.")
+
+
+
+
+
+
+# import sys
+# import os
+
+# # Adiciona o diretório raiz do projeto ao sys.path
+# current_dir = os.path.dirname(__file__)
+# root_dir = os.path.abspath(os.path.join(current_dir, '..'))
+# sys.path.insert(0, root_dir)
+
+# from fastapi import FastAPI, HTTPException
+# from fastapi.openapi.docs import get_swagger_ui_html
+# from starlette.responses import RedirectResponse, JSONResponse
+
+# from src.config.log_config import log_config
+# from src.controller.auth_controller import auth_router
+# from src.controller.product_controller import product_router
+
+# app = FastAPI(
+#     title="Product Registration API",
+#     description="Api to manage products",
+#     version="1.0.0",
+#     openapi_url="/openapi.json",
+#     docs_url=None,
+#     redoc_url=None,
+#     contact={
+#         "name": "Luan Fernandes",
+#         "email": "luan.santos26@fatec.sp.gov.br",
+#         "url": "https://luanfernandes.dev"
+#     },
+#     license_info={
+#         "name": "MIT",
+#         "url": "https://opensource.org/licenses/MIT"
+#     },
+#     servers=[
+#         {
+#             "url": "http://localhost:8000",
+#             "description": "Development server"
+#         },
+#         {
+#             "url": "http://user.luanfernandes.dev",
+#             "description": "Production server"
+#         }
+#     ]
+# )
+
+# app.include_router(product_router)
+# app.include_router(auth_router)
+
+
+# @app.exception_handler(Exception)
+# async def general_exception_handler(request, exc: Exception):
+#     return JSONResponse(
+#         status_code=500,
+#         content={"message": "Um erro inesperado correu no servidor"}
+#     )
+
+
+# @app.exception_handler(HTTPException)
+# async def general_exception_handler(request, exc: HTTPException):
+#     return JSONResponse(
+#         status_code=exc.status_code,
+#         content={"message": exc.detail}
+#     )
+
+
+# @app.get('/', tags=['Redirect'], include_in_schema=False)
+# async def redirect_to_docs():
+#     return RedirectResponse(url='/docs')
+
+
+# @app.get('/docs', tags=['Redirect'], include_in_schema=False)
+# async def get_openapi():
+#     return get_swagger_ui_html(
+#         openapi_url="/openapi.json",
+#         title="OpenApi UI"
+#     )
+
+
+# if __name__ == '__main__':
+#     import uvicorn
+
+#     uvicorn.run(app, host='localhost', port=8000, log_config=log_config)
